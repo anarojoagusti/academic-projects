fedcaf5e4b6a6f5f40a18296eaa855c5b480ad5d
anarojoagusti <38491556+anarojoagusti@users.noreply.github.com>
2018-04-18 11:41:36 +0200

    Update cnn_bioinf.py

--- cnn_bioinf.py
+++ cnn_bioinf.py
@@ -89,36 +89,5 @@ classifier.fit_generator(train_set,
                          nb_val_samples = 200)
                          #use_multiprocessing=True
 
-## Part 3 - Predict labels for new test sets 
-file=open(pathwork + 'predict_set\\Labels.csv','r')
-fileLabels=csv.reader(file)
-labels=list()
-for line in fileLabels:
-    labels=line
-file.close()
-pred_labels=[int(i) for i in labels]
-    
-filesImages=os.listdir()
-del filesImages[filesImages.index('Labels.csv')]
-labelsCNN=list(range(len(filesImages)))
-for i in range(len(filesImages)):
-    test_image = image.load_img(filesImages[i], target_size = (64, 64))
-    test_image = image.img_to_array(test_image)
-    test_image = np.expand_dims(test_image, axis = 0)
-    result = classifier.predict(test_image)
-    indices=train_set.class_indices
-    labelsCNN[i]=result.argmax()
-       
-accuracy=accuracy_score(pred_labels, labelsCNN)
-recall=recall_score(pred_labels,labelsCNN,average='weighted')   
-precision=precision_score(pred_labels,labelsCNN,average='weighted')   
-    
-#if result[0][0] == 0:
-#prediction = 'flower'
-#if result[0][0] == 1:
-#prediction = 'fruit'
-#if result[0][0] == 2:
-#prediction = 'leaf'
-#if result[0][0] == 3:
-#prediction = 'trichoma'
+
 
